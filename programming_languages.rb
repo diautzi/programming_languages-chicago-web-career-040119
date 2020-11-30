c65eb513c681d2bb282fce2c5c3b8aa05025f433
Diana Utan <didiutzi@gmail.com>
2020-11-30 22:54:57 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -42,7 +42,7 @@ def reformat_languages(languages)
         # binding.pry
         if language == :javascript
           new_hash[language] = {
-            :type => attributes,
+            :type => atributes,
             :style => [:oo, :functional]
           }
         else
