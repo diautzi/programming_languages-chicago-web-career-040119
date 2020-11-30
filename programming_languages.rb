4653d936f8f2d66f5896415ed0bba4550dd9f549
Diana Utan <didiutzi@gmail.com>
2020-11-30 22:49:54 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,42 @@
+require 'pry'
+
+hash = {
+  :ruby => {
+    :type => "interpreted",
+    :style => [:oo]
+  },
+  :javascript => {
+    :type => "interpreted",
+    :style => [:oo, :functional]
+  },
+  :python => {
+    :type => "interpreted",
+    :style => [:oo]
+  },
+  :java => {
+    :type => "compiled",
+    :style => [:oo]
+  },
+  :clojure => {
+    :type => "compiled",
+    :style => [:functional]
+  },
+  :erlang => {
+    :type => "compiled",
+    :style => [:functional]
+  },
+  :scala => {
+    :type => "compiled",
+    :style => [:functional]
+  }
+}
+
+
 def reformat_languages(languages)
   # your code here
   new_hash = {}
   languages.each do |language_details, language|
+    binding.pry
     language.each do |language, attributes|
       attributes.each do |attribute, attribute_value|
         if language == :javascript
@@ -19,5 +54,6 @@ def reformat_languages(languages)
     end
   end
   new_hash
-
 end
+
+reformat_languages(hash)
